56923951ec5b3f4b529d71136f43894f8c334097
saraartan <saraartann@gmail.com>
2026-08-10 14:46:23 -0400

    Update OIDC trust policy for repo rename to it-tools-ecs

--- infra/github-oidc.tf
+++ infra/github-oidc.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:saraartan@*/it-tools-devops-project@*:*"
+          "token.actions.githubusercontent.com:sub" = "repo:saraartan@*/it-tools-ecs@*:*"
         }
       }
     }]
